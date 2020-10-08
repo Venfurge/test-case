883f5c136df4b0a8dcc46e3cbc9c1accc331a25e
ZheniaB <59691258+Venfurge@users.noreply.github.com>
2020-10-08 19:46:10 +0300

    Added Exception classes and ControllerBase

---
+++ TestCase/TestCase/Controllers/AppControllerBase.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using TestCase.Exceptions;
+
+namespace TestCase.Controllers
+{
+    public class AppControllerBase : ControllerBase
+    {
+        protected async Task<ActionResult<T>> ExecuteWithOkResponse<T>(Func<Task<T>> func)
+        {
+            try
+            {
+                return Ok(await func());
+            }
+            catch (NotFoundException e)
+            {
+                return NotFound(e.ToString());
+            }
+            catch (BadRequestException e)
+            {
+                return BadRequest(e.ToString());
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        protected async Task<ActionResult> ExecuteWithOkResponse(Func<Task> func)
+        {
+            try
+            {
+                await func();
+                return Ok();
+            }
+            catch (NotFoundException e)
+            {
+                return NotFound(e.ToString());
+            }
+            catch (BadRequestException e)
+            {
+                return BadRequest(e.ToString());
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
